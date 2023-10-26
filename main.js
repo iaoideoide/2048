36d69d0bc4c6819f2b040189066fa67353fc0b62
Iair Steimberg <iairbarak@gmail.com>
2023-10-25 23:51:15 -0300

    se estilizo el perdiste y se hizo la funcion volver

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var tablero, puntos = 0, filas = 4, columnas = 4;
+var tablero, puntos = 0, filas = 4, columnas = 4, oldTablero;
 const pPuntos = document.getElementById("puntaje");
 
 document.addEventListener("DOMContentLoaded", ()=> {
@@ -46,11 +46,21 @@ function dos() {
         let f = Math.floor(Math.random() * filas);
         let c = Math.floor(Math.random() * columnas);
         if (tablero[f][c] == 0) {
-            tablero[f][c] = 2;
-            let cuadro = document.getElementById(f.toString() + "-" + c.toString());
-            cuadro.innerText = "2";
-            cuadro.classList.add("x2");
-            hay = true;
+            let n = Math.floor(Math.random()*10)
+            if(n%2 == 0){
+                tablero[f][c] = 2;
+                let cuadro = document.getElementById(f.toString() + "-" + c.toString());
+                cuadro.innerText = "2";
+                cuadro.classList.add("x2");
+                hay = true;
+            }else{
+                tablero[f][c] = 4;  
+                let cuadro = document.getElementById(f.toString() + "-" + c.toString());
+                cuadro.innerText = "4";
+                cuadro.classList.add("x4");
+                hay = true;
+            }
+            
         }
     }
 }
@@ -67,31 +77,34 @@ function vacio() {
 }
 
 document.addEventListener("keydown", (event) => {
-       ;
 	switch (event.keyCode) {
 		case 37:
+            guardarEstadoActual();
 			LEFT();
             break;
 		case 39:
+            guardarEstadoActual();
 			RIGHT();
 			break;
 		case 38:
+            guardarEstadoActual();
 			UP();
 			break;
 		case 40:
-			DOWN();
+            guardarEstadoActual();    
+            DOWN();
 			break;
 		default:
 			break;
 	}
-    if (vacio()) {
-        dos();
-    }
-
-    if (!vacio() && noMasMovimientos()) {
-        console.log("perdiste");
-        perdiste();
-    }
+    if (vacio()) {   
+                dos();
+            }
+        
+            if (!vacio() && noMasMovimientos()) {
+                console.log("perdiste");
+                perdiste();
+            }
 
 
 });
@@ -194,7 +207,53 @@ function noMasMovimientos() {
     return true;
 }
 
-function perdiste(){
-    let tablero = document.getElementById("tablero");
-    tablero.classList.add("perdio")
+function perdiste() {
+    console.log("perdiste");
+    document.querySelector(".blur").style.display = "flex"; 
+    document.getElementById("perdiste").style.display = "block"; 
+}
+
+
+function volver() {
+    if (oldTablero) {
+        for (let f = 0; f < filas; f++) {
+            for (let c = 0; c < columnas; c++) {
+                tablero[f][c] = oldTablero[f][c];
+                let cuadro = document.getElementById(f.toString() + "-" + c.toString());
+                let num = tablero[f][c];
+                actualizar(cuadro, num);
+            }
+        }
+    }
+    document.querySelector(".blur").style.display = "none";
+    document.getElementById("perdiste").style.display = "none";
+}
+
+function guardarEstadoActual() {
+    oldTablero = tablero.map(row => row.slice());
+}
+
+function reiniciar() {
+    tablero = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+    dos();
+    dos();
+    
+    for (let f = 0; f < filas; f++) {
+        for (let c = 0; c < columnas; c++) {
+            let cuadro = document.getElementById(f.toString() + "-" + c.toString());
+            cuadro.innerText = "";
+            actualizar(cuadro, 0);
+        }
+    }
+
+    puntos = 0;
+    pPuntos.innerText = puntos;
+
+    document.querySelector(".blur").style.display = "none";
+    document.getElementById("perdiste").style.display = "none";
 }
